db31a124972574016b589dfa3bbe993b23fb6c13
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 17:45:07 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash ={}
+  languages.each do |style, style_values|
+    style_values.each do |lang, lang_info|
+      # defining all the value and parts of the hash 
+      if !new_hash.include?(lang) #setting up the new hash with language first 
+        new_hash[lang] = { 
+          :type => lang_info[:type],
+          :style => [style]
+}            # if theres no type then the else statement pushes out the hash in the correct order.
+    else new_hash[lang][:style].push (style)
+  end 
+end 
+end 
+
+
+puts new_hash
+return new_hash
 end
