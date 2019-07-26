2b532ed1786ed84ed858999df02e6e414e1a6842
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 17:36:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,6 @@ def reformat_languages(languages)
   new_hash ={}
   languages.each do |style, style_values|
     style_values.each do |lang, lang_info|
-      if !new_hash.include?
+      if !new_hash.include?(lang)
+        
 end
